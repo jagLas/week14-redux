c89acc182d1edf01a92abf476f150e13493b07de
jagLas <jglasher@gmail.com>
2023-05-12 16:39:09 -0400

    phase 7 complete

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
@@ -1,6 +1,7 @@
 import produceData from '../mockData/produce.json'
 
 const POPULATE = 'produce/POPULATE'
+const TOGGLE_LIKE = 'produce/TOGGLE_LIKE'
 
 export const populateProduce = () => {
     return {
@@ -9,14 +10,31 @@ export const populateProduce = () => {
     }
 }
 
+export const toggleLike = (id) => {
+    return {
+        type: TOGGLE_LIKE,
+        id
+    }
+}
+
 function produceReducer (state = {}, action ){
     switch(action.type) {
-        case POPULATE:
+        case POPULATE:{
             const nextState = {}
             action.produce.forEach(produce => {
                 nextState[produce.id] = {...produce}
             })
             return nextState;
+        }
+        case TOGGLE_LIKE: {
+            const {id} = action;
+            const newState = {...state};
+            newState[id] = {
+                ...newState[id],
+            }
+            newState[id].liked = !state[id].liked
+            return newState;
+        }
         default:
             return state;
     }
