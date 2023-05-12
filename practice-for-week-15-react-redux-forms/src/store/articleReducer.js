e4de3ef551c075ddbef967e3672ecc81b4c97c23
jagLas <jglasher@gmail.com>
2023-05-12 13:04:33 -0400

    practice complete

--- practice-for-week-15-react-redux-forms/src/store/articleReducer.js
+++ practice-for-week-15-react-redux-forms/src/store/articleReducer.js
@@ -1,6 +1,7 @@
 import articles from '../data/data.json';
 
 const LOAD_ARTICLES = 'article/loadArticles';
+const ADD_ARTICLE = 'article/addArticle';
 
 export const loadArticles = () => {
   return {
@@ -16,9 +17,19 @@ const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
       return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      return {...state, entries: [...state.entries, action.article]}
     default:
       return state;
   }
 };
+
+
+export const addArticle = (article) => {
+  return {
+    type: ADD_ARTICLE,
+    article
+  }
+}
 
 export default articleReducer;
